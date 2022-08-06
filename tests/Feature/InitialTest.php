c26e664841cdc7f606a52057dc2a60e2e2fa8c47
Valeh ASADLI <valehasadli@gmail.com>
2022-08-06 19:41:28 +0200

    initial test

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ValehAsadli\Kafka\Tests\Feature;
+
+use Orchestra\Testbench\TestCase;
+
+class InitialTest extends TestCase
+{
+    /** @test */
+    public function welcome_test(): void
+    {
+        $this->assertTrue(true);
+    }
+}
